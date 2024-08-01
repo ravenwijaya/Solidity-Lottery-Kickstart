ee440191c1bc8b4daa863dca0326d07823a61644
Raven Wijaya <ravenwijaya@MacBook-Pro.local>
2024-08-01 16:34:20 +0700

    add env

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 
-const instance = new web3.eth.Contract(CampaignFactory.abi, "FACTORY_ADDRESS");
+const instance = new web3.eth.Contract(CampaignFactory.abi, process.env.NEXT_PUBLIC_FACTORY_ADDRESS);
 
 export default instance;
